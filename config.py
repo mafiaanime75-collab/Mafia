38b86bff862768b38557afac2a0191edb72121ef
mafiaanime75-collab <mafiaanime75@gmail.com>
2026-07-14 19:46:47 +0500

    Update config.py

--- config.py
+++ config.py
@@ -5,13 +5,13 @@ Token va guruh linkini shu yerga (yoki muhit o'zgaruvchilariga) kiriting.
 import os
 
 # Bot tokeningizni shu yerga yozing yoki BOT_TOKEN muhit o'zgaruvchisidan oling
-BOT_TOKEN = os.getenv("BOT_TOKEN", "SIZNING_BOT_TOKENINGIZ_BU_YERGA")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8613383536:AAFtxKbwkbo6jwpU_AI-oHdoHLstw-8DcjE")
 
 # Asosiy o'yin o'ynaladigan guruh (lobby'dan keyin o'yinchilar shu yerga o'tadi)
-MAIN_GROUP_ID = int(os.getenv("MAIN_GROUP_ID", "-1000000000000"))
-MAIN_GROUP_INVITE_LINK = os.getenv("MAIN_GROUP_INVITE_LINK", "https://t.me/sizning_guruhingiz")
+MAIN_GROUP_ID = int(os.getenv("MAIN_GROUP_ID", "-5305326142"))
+MAIN_GROUP_INVITE_LINK = os.getenv("MAIN_GROUP_INVITE_LINK", "https://t.me/+j5SvFE1hj75iNzUy")
 
-DB_PATH = os.getenv("DB_PATH", "anime_mafia.db")
+DB_PATH = os.getenv("DB_PATH", "AniMafi_UZ_bot")
 
 # O'yin sozlamalari
 MIN_PLAYERS = 5
@@ -21,5 +21,5 @@ DAY_DISCUSSION_SEC = 90
 VOTING_DURATION_SEC = 40
 
 # Valyuta nomi (o'zgaruvchan qilib chiqarildi - kerak bo'lsa almashtiring)
-CURRENCY_NAME = "Kizuna"
+CURRENCY_NAME = "sehirli tosh"
 CURRENCY_SYMBOL = "🔗"
