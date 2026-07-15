4540027ca87253420745898c6ffdbb6200ec79c9
mafiaanime75-collab <mafiaanime75@gmail.com>
2026-07-15 15:12:51 +0500

    Update states.py

--- states.py
+++ states.py
@@ -2,4 +2,10 @@ from aiogram.fsm.state import State, StatesGroup
 
 
 class WorldSelect(StatesGroup):
+    """Lobby ochilib, HOST 'boshlash'ni bosganda ishlaydigan holat."""
     waiting_query = State()
+
+
+class FeedbackFlow(StatesGroup):
+    waiting_kind = State()
+    waiting_text = State()
